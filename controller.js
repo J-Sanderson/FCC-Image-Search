dc410f8d5199dcde8a607c58027832974edce404
System <none>
2017-09-19 16:30:22 +0000

    save searches to database

--- controller.js
+++ controller.js
@@ -1,16 +1,26 @@
 
 var Flickr = require("node-flickr");
+var mongoose = require('mongoose');
 var keys = {"api_key": process.env.KEY}
+
+mongoose.connect('mongodb://' + process.env.ID + ':' + process.env.PW + '@ds157233.mlab.com:57233/search');
+var searchSchema = new mongoose.Schema({
+  term: String,
+  when: Object
+});
+var Searchmodel = mongoose.model('search', searchSchema);
 
 module.exports.searchResults = function(searchVal, offset, res) {
   var flickr = new Flickr(keys);
   flickr.get("photos.search", {"tags": searchVal}, function(err, result){
     if (err) throw err;
-    console.log(result.photos.photo);
+    //console.log(result.photos.photo);
     //create array to hold results to return
     var resList = [];
-    //fix errors if offset < 1
-    if (offset < 1) { offset = 1};
+    //default to 1st page if offset < 1 || undefined
+    if (offset < 1 || typeof offset === 'undefined') {
+      offset = 1
+    };
     //get first ten results
     for (var i = 10 * (offset-1); i < 10 * offset; i++) {
       resList.push({
@@ -18,8 +28,13 @@ module.exports.searchResults = function(searchVal, offset, res) {
         title: result.photos.photo[i].title
       })
     }
-    //return array
-    res.setHeader('Content-Type', 'application/json');
-    res.send(resList);
+    //add to database
+    var now = new Date();
+    var newSearch = Searchmodel({term: searchVal, when: now}).save(function(err, data) {
+      if (err) throw err;
+      //return array
+      res.setHeader('Content-Type', 'application/json');
+      res.send(resList);
+    });
   });
 }
