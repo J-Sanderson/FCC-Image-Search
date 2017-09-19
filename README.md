e3de441e2228c32637b95b5913a6e557d9cf4e0f
System <none>
2017-09-19 15:54:21 +0000

    add pagination to search

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ Image Search Abstraction Layer
 =========================
 
 Created for freeCodeCamp
-
-[Live version on Glitch.com](https://imgsearch-jsanderson.glitch.me/)
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.get("/", function (req, res) {
 });
 
 app.get("/search/:term", function(req, res) {
-  controller.searchResults(req.params.term, res);
+  //paginate next 10 with ?offset=2
+  //console.log(req.query.offset);
+  controller.searchResults(req.params.term, req.query.offset, res);
 })
 
 app.get("/recent", function(req, res) {
